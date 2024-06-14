1b43d116aee47c20c3fe443cbec03e58325f6ed0
crypticsymmetry <ccbb121@gmail.com>
2024-06-14 14:49:40 -0700

    Add files via upload

--- udp_bitmap_decoder.py
+++ udp_bitmap_decoder.py
@@ -1,56 +1,76 @@
-from prot_http.const_wifi import UDP_PORT_LIVEVIEW
 import socket
+import os
+from prot_http.const_wifi import UDP_PORT_LIVEVIEW
 
-with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
-    print("Started receiver!")
+def initialize_socket():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind(('', UDP_PORT_LIVEVIEW))
     sock.settimeout(2)
+    return sock
 
-    data = bytearray(b'')
-    data_idx_frame = None
-    data_idx_last_packet = -1
-    data_valid : bool = True
-    was_last_frame_invalid : bool = False
+def process_packet(packet, data, frame_info):
+    if len(packet) < 12:
+        return False
 
-    try:
-        while True:
-            try:
-                pack, addr = sock.recvfrom(1024000)
-                
-                if len(pack) < 12:
-                    continue
+    idx_frame = int.from_bytes(packet[:4], byteorder='big')
+    len_packet_frame = int.from_bytes(packet[4:8], byteorder='big')
+    idx_packet_frame = int.from_bytes(packet[8:12], byteorder='big')
 
-                idx_frame           = int.from_bytes(pack[:4], byteorder='big')
-                len_packet_frame    = int.from_bytes(pack[4:8], byteorder='big')
-                idx_packet_frame    = int.from_bytes(pack[8:12], byteorder='big')
+    if frame_info['data_idx_frame'] != idx_frame:
+        frame_info.update({
+            'data': bytearray(),
+            'data_idx_frame': idx_frame,
+            'data_idx_last_packet': -1,
+            'data_valid': True,
+        })
 
-                # If the reconstructed frame is not the delivered one, start reconstruction again
-                if data_idx_frame != idx_frame:
-                    data = bytearray(b'')
-                    data_idx_frame = idx_frame
-                    data_idx_last_packet = -1
-                    data_valid = True
-                
-                if data_valid:
-                    if (idx_packet_frame - 1) == data_idx_last_packet:
-                        data.extend(pack[12:])
-                        data_idx_last_packet = idx_packet_frame
-                    else:
-                        data_valid = False
-                        continue
+    if frame_info['data_valid']:
+        if (idx_packet_frame - 1) == frame_info['data_idx_last_packet']:
+            frame_info['data'].extend(packet[12:])
+            frame_info['data_idx_last_packet'] = idx_packet_frame
+        else:
+            frame_info['data_valid'] = False
+            return False
 
-                    if (data_idx_last_packet == len_packet_frame - 1):
-                        if len(data) > 2048:
-                            with open("packet_%d.txt" % data_idx_frame, 'wb') as out:
-                                out.write(data[:2048])
-                            with open("packet_%d.jpg" % data_idx_frame, 'wb') as out:
-                                out.write(data[2048:])
-                        else:
-                            with open("packet_unk_%d.raw" % data_idx_frame, 'wb') as out:
-                                out.write(data)
+        if frame_info['data_idx_last_packet'] == len_packet_frame - 1:
+            save_frame(frame_info['data'], frame_info['data_idx_frame'])
+            return True
 
-            except TimeoutError:
-                print("Timed out...")
+    return False
 
-    except KeyboardInterrupt:
-        pass
+def save_frame(data, idx_frame):
+    if len(data) > 2048:
+        jpg_data = data[2048:]
+        file_name = f"packet_{idx_frame}.jpg"
+        with open(file_name, 'wb') as f:
+            f.write(jpg_data)
+        print(f"Frame saved as {file_name}")
+    else:
+        raw_data = data
+        file_name = f"packet_unk_{idx_frame}.raw"
+        with open(file_name, 'wb') as f:
+            f.write(raw_data)
+        print(f"Frame saved as {file_name}")
+
+def receive_packets():
+    with initialize_socket() as sock:
+        print("Started receiver!")
+        frame_info = {
+            'data': bytearray(),
+            'data_idx_frame': None,
+            'data_idx_last_packet': -1,
+            'data_valid': True,
+        }
+
+        try:
+            while True:
+                try:
+                    packet, _ = sock.recvfrom(1024000)
+                    process_packet(packet, frame_info['data'], frame_info)
+                except socket.timeout:
+                    print("Timed out...")
+        except KeyboardInterrupt:
+            pass
+
+if __name__ == "__main__":
+    receive_packets()
